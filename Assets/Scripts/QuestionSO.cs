71cc24d289edd5147a10eaa96c33544afd2bad2c
DevTruce <DevTruce@gmail.com>
2023-10-05 23:06:35 -0400

    completed questions and answers

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,8 +6,27 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject 
 {
     ////////////////////////////////////////////
-    //// 
+    //// Creating input for questions 
     
     [TextArea(2, 6)] // min and max amount of characters
     [SerializeField] string question = "Enter New Question Text Here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    ////////////////////////////////////////////
+    //// Getter Method (read only access)
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
