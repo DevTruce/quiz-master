bad2d8c4d9e6d71732087d0b4011f113a8d66765
DevTruce <DevTruce@gmail.com>
2023-10-05 22:43:55 -0400

    created questions, prefabs and scriptable objects

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question", order = 0)]
+public class QuestionSO : ScriptableObject 
+{
+    ////////////////////////////////////////////
+    //// 
+    
+    [TextArea(2, 6)] // min and max amount of characters
+    [SerializeField] string question = "Enter New Question Text Here";
+}
