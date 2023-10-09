d57fba2da88fb437cbd1d9711cfe68d45f68a4ef
DevTruce <DevTruce@gmail.com>
2023-10-08 21:26:50 -0400

    minor update to ui

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question", order = 0)]
-public class QuestionSO : ScriptableObject 
+public class QuestionSO : ScriptableObject
 {
     ////////////////////////////////////////////
-    //// Creating input for questions 
-    
+    //// Creating input for questions
+
     [TextArea(2, 6)] // min and max amount of characters
-    [SerializeField] string question = "Enter New Question Text Here";
-    [SerializeField] string[] answers = new string[4];
-    [SerializeField] int correctAnswerIndex; 
+    [SerializeField]
+    string question = "Enter New Question Text Here";
+
+    [SerializeField]
+    string[] answers = new string[4];
+
+    [SerializeField]
+    int correctAnswerIndex;
+
     ////////////////////////////////////////////
     //// Getter Method (read only access)
     public string GetQuestion()
--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
+
     [SerializeField] QuestionSO question;
+
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+
     [SerializeField] Sprite defaultAnswerSprite;
+
     [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
@@ -18,8 +22,7 @@ public class Quiz : MonoBehaviour
         // find and display question
         questionText.text = question.GetQuestion();
 
-
-       // find and display possible answers 
+        // find and display possible answers
         for (int i = 0; i < answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
@@ -27,24 +30,23 @@ public class Quiz : MonoBehaviour
         }
     }
 
-    public void onAnswerSelected(int index) 
+    public void onAnswerSelected(int index)
     {
         Image buttonImage;
 
-        // correct answer
-        if (index == question.GetCorrectAnswerIndex())
+        if (index == question.GetCorrectAnswerIndex()) // correct answer
         {
             questionText.text = "Correct answer";
-            buttonImage = answerButtons[index].GetComponent<Image>(); // get image component for selected button 
+            buttonImage = answerButtons[index].GetComponent<Image>(); // get image component for selected button
             buttonImage.sprite = correctAnswerSprite; // update button sprite
-        } else 
+        }
+        else // incorrect answer
         {
             correctAnswerIndex = question.GetCorrectAnswerIndex(); // get the correct answer
-            string correctAnswer = question.GetAnswer(correctAnswerIndex); // store the correct answer 
-            questionText.text = "Sorry, the correct answer was: \n" + correctAnswer; 
+            string correctAnswer = question.GetAnswer(correctAnswerIndex); // store the correct answer
+            questionText.text = "Sorry, the correct answer was: \n" + correctAnswer;
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>(); // get image component for selected button
             buttonImage.sprite = correctAnswerSprite; // update button sprite
-
         }
     }
 }
